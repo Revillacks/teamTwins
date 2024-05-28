832cde5951547ce15bb47baa2e8ecca5a4b01a8f
V1ctorHu <156713077+V1ctorHu@users.noreply.github.com>
2024-05-28 09:07:49 -0600

    servicios agregados

--- src/app/services/rickandmorty.service.ts
+++ src/app/services/rickandmorty.service.ts
@@ -2,31 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RickandmortyService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) {
-
+  public getCharacter() {
+    return this.http.get('https://rickandmortyapi.com/api/character');
   }
 
-    public getCharacter(){
-      return this.http.get('https://rickandmortyapi.com/api/character')
-    }
+  public getEpisode() {
+    return this.http.get('https://rickandmortyapi.com/api/episode');
+  }
 
-    public getEpisode(){
-      return this.http.get('https://rickandmortyapi.com/api/episode')
-    }
+  public getLocation() {
+    return this.http.get('https://rickandmortyapi.com/api/location');
+  }
 
-    public getLocation(){
-      return this.http.get('https://rickandmortyapi.com/api/location')
-    }
+  public getCharacterById(id: number) {
+    return this.http.get('https://rickandmortyapi.com/api/character/${id}');
+  }
 
-    public getCharacterById(id: number){
-      return this.http.get('https://rickandmortyapi.com/api/character/${id}')
-    }
+  public getEpisodeById(id: number) {
+    return this.http.get('https://rickandmortyapi.com/api/episode/${id}');
+  }
 
-
+  public getPokemon() {
+    return this.http.get('https://pokeapi.co/api/v2/pokemon');
+  }
+  public getPokemonById(id: number) {
+    return this.http.get('https://pokeapi.co/api/v2/pokemon/${id}');
+  }
 }
